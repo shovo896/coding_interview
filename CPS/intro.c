a8ec08d10bb00a8e30d37bf9421be4d03a8925b1
Shuvo <suvo10.info@gmail.com>
2026-04-30 21:55:23 +0600

    hb

--- CPS/intro.c
+++ CPS/intro.c
@@ -2,7 +2,7 @@
 int main()
 {
     printf("Hello World\n");  // a small footstep that will be ur life one day 
-    printf("Welcome to C Programming\n");
+    printf("Welcome to C Programming\n ");
     printf("Hello\"Bangladesh\"\n");  // this is a comment
     return 0;
 }
