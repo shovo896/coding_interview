454e3fc6e9b72256150039f188604a1228951281
Shuvo <suvo10.info@gmail.com>
2026-04-30 21:44:44 +0600

    g.mklvm

--- CPS/intro.c
+++ CPS/intro.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    printf("Hello World");  // a small footstep that will be ur life one day 
+    printf("Hello World\n");  // a small footstep that will be ur life one day 
+    printf("Welcome to C Programming\n");
     return 0;
 }
