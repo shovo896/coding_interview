da240c35ec09061dea6e64aad68cc8374c7275d5
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-06-11 22:33:17 +0600

    bjhbn

--- CPS/hnbjk.c
+++ CPS/hnbjk.c
@@ -6,7 +6,7 @@ int main(){
         while (spaces--){
             printf(" ");
         }
-        for (int j=0 ;j<2*i-1;j++){
+        for (int j=i ;j>=1;j--){
             printf("%d\n",j);
         }
         printf("\n");
