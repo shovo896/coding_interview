599ea2c25b255538bc9d89c655c7b410f6ba27ed
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-06-11 22:24:28 +0600

    bjmn

--- CPS/hnbjk.c
+++ CPS/hnbjk.c
@@ -1,16 +1,15 @@
 #include<stdio.h> 
 int main(){
     int n= 5 ;
-    scanf("%d", &n);
-    for (int i=n-1 ;i<=0;i++){
-        for (int j=0 ;j<=i;j++){
-            printf("* ");
+    for (int i = 1 ; i<=n;i++){
+        int spaces = n-i;
+        while (spaces--){
+            printf(" ");
+        }
+        for (int j=0 ;j<2*i-1;j++){
+            printf("*");
         }
         printf("\n");
-        
-        
-
-    
     }
-    return 0;
-}
+    return 0;   
+    }
