9db3f6dad9b2728f4d3a643656417c42bede8961
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-06-11 22:16:55 +0600

    hnbkjj

--- CPS/hnbjk.c
+++ CPS/hnbjk.c
@@ -2,8 +2,8 @@
 int main(){
     int n= 5 ;
     scanf("%d", &n);
-    for (int i=0 ;i<=n;i--){
-        for (int j=0 ;j<=i;j--){
+    for (int i=n-1 ;i<=0;i++){
+        for (int j=0 ;j<=i;j++){
             printf("* ");
         }
         printf("\n");
