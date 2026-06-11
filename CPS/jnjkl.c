4e9a870a0e193086d2a73003f2e2588bb15f33c8
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-06-11 23:17:54 +0600

    bjknnkm

---
+++ CPS/jnjkl.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n=100;
+    for (int i =2 ;i<=n ;i++){
+        int cnt = 0 ;
+        for (int j = 2 ;j<i;j++){
+            if (i%j==0){
+                cnt++;
+                break ;
+            }
+        }
+        if (cnt==0){
+            printf("%d\n",i);
+        }
+    }
+}
