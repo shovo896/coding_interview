ae23cabff42d17ed2d131133bbb71c09afac3ea7
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-06-11 21:44:37 +0600

    chbbhj

--- CPS/bgkml.c
+++ CPS/bgkml.c
@@ -1,17 +1,8 @@
-#include<stdio.h>
+#include<stdio.h> 
 int main(){
-    int x ;   //.  `eta hobe na at the end 
-    scanf("%d",&x);
-    int sum = 0 ;
-    while(x!=0){
-        int last = x % 10 ;
-        sum += last ;
-        printf("last %d sum= %d\n",last,sum);
-
-        x/= 10 ;
-
+    int n= 5 ;
+    scanf("%d", &n);
+    for (int i=0 ;i<=n;i++){
+        printf("%d ",i);
     }
-    printf("%d\n",sum);
-    return 0 ;
-
 }
