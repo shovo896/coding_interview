0217c1f409c071849ca26ba6e74871432aa441c2
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-10 12:48:26 +0600

    fkljvl

--- CPS/function.c
+++ CPS/function.c
@@ -17,6 +17,15 @@ int f2(int x,double y){
     return x+y ;
 }
 
+int max(int a,int b){
+    if (a>b){
+        return a ;
+    }
+    else{
+        return b ;
+    }
+}
+
 int main(){
     int test = f(4);
     printf("%d\n",test);
@@ -26,5 +35,7 @@ int main(){
     printf("%d\n",test3);
     int test4 = f2(3,5.0);
     printf("%d\n",test4);
+    int test5 = max(3,5);
+    printf("%d\n",test5);
     return 0 ;
 }
