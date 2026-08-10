af13d9c37e239e51beddb428c35448b8f736bfb9
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-10 12:26:16 +0600

    ijiok

--- CPS/function.c
+++ CPS/function.c
@@ -3,8 +3,15 @@ int f(int x ){
     int ans = 3*x + 2 ;
     return ans ;
 }
+
+// double 
+double d(double y ){
+    return y/2 ; 
+}
 int main(){
     int test = f(4);
     printf("%d\n",test);
+    double test2 = d(4.0);
+    printf("%f\n",test2);
     return 0 ;
 }
