38848dbd32d85bcbabd2d509065976b93cf4bba1
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-10 12:29:02 +0600

    bhkj

--- CPS/function.c
+++ CPS/function.c
@@ -8,10 +8,16 @@ int f(int x ){
 double d(double y ){
     return y/2 ; 
 }
+
+int doubleToInt(double y){
+    return floor(y); 
+}
 int main(){
     int test = f(4);
     printf("%d\n",test);
     double test2 = d(4.0);
     printf("%f\n",test2);
+    int test3 = doubleToInt(4.5);
+    printf("%d\n",test3);
     return 0 ;
 }
